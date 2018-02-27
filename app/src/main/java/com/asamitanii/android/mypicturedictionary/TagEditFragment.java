db956a71bdc077ae249b75377a8935b15136705c
asamitanii <g033.9521@gmail.com>
2018-02-26 17:46:05 -0800

    used setView for a custom layout as a dialog

--- app/src/main/java/com/asamitanii/android/mypicturedictionary/TagEditFragment.java
+++ app/src/main/java/com/asamitanii/android/mypicturedictionary/TagEditFragment.java
@@ -1,9 +1,11 @@
 package com.asamitanii.android.mypicturedictionary;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 
 /**
  * Created by tanii_asami on 2/26/18.
@@ -12,6 +14,25 @@ import android.support.v7.app.AlertDialog;
 public class TagEditFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getActivity()).setTitle(R.string.tag_edit_dialog_title).setPositiveButton(android.R.string.ok, null).create();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle(R.string.tag_edit_dialog_title);
+
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_tag_edit, null));
+
+        // Add action buttons
+           builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // add a new tag here...
+            }
+        }).setNegativeButton(R.string.cancel, null);
+
+        return builder.create();
     }
 }
